5cfa7be3d56dfc6d15b6617d32b0172989eaef10
William Falk-Wallace <wgf2104@columbia.edu>
2015-02-11 17:30:03 -0800

    adds fresh

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '8.0'
+
+target 'howl' do
+
+  pod 'AFNetworking', '~> 2.5.0'
+  pod 'MRProgress', '~> 0.8.1'
+  pod 'SwiftyJSON', '2.1.3'
+  pod 'Alamofire', '~> 1.1'
+  pod 'BDBOAuth1Manager', "~> 1.5.0"
+
+end
+
+target 'howlTests' do
+
+end
---
+++ howl/howl-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <AFNetworking/UIImageView+AFNetworking.h>
+#import <BDBOAuth1Manager/NSDictionary+BDBOAuth1Manager.h>
+#import <BDBOAuth1Manager/BDBOAuth1RequestOperationManager.h>
