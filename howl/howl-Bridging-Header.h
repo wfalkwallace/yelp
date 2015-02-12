a591147ce4977ff63cce0528a649c2d2ac2f7a75
William Falk-Wallace <wgf2104@columbia.edu>
2015-02-11 22:33:01 -0800

    cleans

--- howl/howl-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <AFNetworking/UIImageView+AFNetworking.h>
-#import <BDBOAuth1Manager/NSDictionary+BDBOAuth1Manager.h>
-#import <BDBOAuth1Manager/BDBOAuth1RequestOperationManager.h>
