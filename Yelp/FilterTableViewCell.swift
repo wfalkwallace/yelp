b263f646318e4f760395145fa1e1b28371024333
William Falk-Wallace <wgf2104@columbia.edu>
2015-02-14 11:56:24 -0800

    adds filter scaffolding

---
+++ Yelp/FilterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterTableViewCell.swift
+//  Yelp
+//
+//  Created by William Falk-Wallace on 2/14/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class FilterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
